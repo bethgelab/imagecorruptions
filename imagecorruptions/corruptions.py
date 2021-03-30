952dc3e04bbed023345179bbf86b856458722f16
Subash Prakash <subashprakash8@gmail.com>
2021-03-30 23:52:17 +0200

    Removed the commented old glass_blur method

--- imagecorruptions/corruptions.py
+++ imagecorruptions/corruptions.py
@@ -204,28 +204,6 @@ def gaussian_blur(x, severity=1):
     return np.clip(x, 0, 1) * 255
 
 
-# def glass_blur(x, severity=1):
-#     # sigma, max_delta, iterations
-#     c = [(0.7, 1, 2), (0.9, 2, 1), (1, 2, 3), (1.1, 3, 2), (1.5, 4, 2)][
-#         severity - 1]
-
-#     x = np.uint8(
-#         gaussian(np.array(x) / 255., sigma=c[0], multichannel=True) * 255)
-#     x_shape = np.array(x).shape
-
-#     # locally shuffle pixels
-#     for i in range(c[2]):
-#         for h in range(x_shape[0] - c[1], c[1], -1):
-#             for w in range(x_shape[1] - c[1], c[1], -1):
-#                 dx, dy = np.random.randint(-c[1], c[1], size=(2,))
-#                 h_prime, w_prime = h + dy, w + dx
-#                 # swap
-#                 x[h, w], x[h_prime, w_prime] = x[h_prime, w_prime], x[h, w]
-
-#     return np.clip(gaussian(x / 255., sigma=c[0], multichannel=True), 0,
-#                    1) * 255
-
-
 def glass_blur(x, severity=1):
     # sigma, max_delta, iterations
     c = [(0.7, 1, 2), (0.9, 2, 1), (1, 2, 3), (1.1, 3, 2), (1.5, 4, 2)][
